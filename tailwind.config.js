8bbc9b25aa199f3b562ac389685f9309b2fbda47
Jan Mikulka <jan.mikulka@email.cz>
2025-03-21 08:37:01 +0100

    Add package maryUI v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,29 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        './node_modules/daisyui/dist/**/*.js',
+        './vendor/mary/ui/resources/**/*.blade.php',
+        './vendor/robsontenorio/mary/src/View/Components/*.php',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+    // theme: {
+    //     extend: {
+    //         fontFamily: {
+    //             sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+    //         },
+    //     },
+    // },
 
-    plugins: [forms, typography],
+    // plugins: [forms, typography, daisyui],
+    plugins: [require('daisyui')],
 };
